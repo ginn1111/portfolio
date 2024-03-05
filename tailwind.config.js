b2db9f9a155b1e55858a5fdbb50dbde9f111a3b6
thuanpv <thuanpv@payme.vn>
2024-03-05 11:30:07 +0700

    feat: implementation hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./index.html'],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
+      colors: {
+        text: {
+          primary: '#9fef03',
+        },
+        primary: '#56f3a2',
+      },
       fontFamily: {
-        sans: ['Exo', 'sans-serif']
-      }
+        sans: ['Exo', 'sans-serif'],
+      },
     },
   },
   plugins: [],
-}
-
+};
