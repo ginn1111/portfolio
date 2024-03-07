3fdce68be767dfa02e482760f0544c85678af0d9
ginn1111 <vanthuanjw@gmail.com>
2024-03-07 23:56:03 +0700

    feat: implementation sides project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
     container: {
       center: true,
+      padding: "2rem",
     },
     extend: {
+      gridTemplateColumns: {
+        "auto-fill-350": "repeat(auto-fill, minmax(350px, 1fr))",
+      },
       colors: {
-        background: '#111927',
+        background: "#111927",
         text: {
-          DEFAULT: '#a4b1cd',
-          primary: '#9fef03',
+          DEFAULT: "#a4b1cd",
+          primary: "#9fef03",
         },
-        primary: '#56f3a2',
+        primary: "#56f3a2",
       },
       fontFamily: {
-        sans: ['Exo', 'sans-serif'],
-        script: ['Pacifico', 'cursive'],
+        sans: ["Exo", "sans-serif"],
+        script: ["Pacifico", "cursive"],
       },
     },
   },
