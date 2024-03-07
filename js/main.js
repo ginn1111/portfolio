28d4098096b4d8177549a435d54647d8c4f9684c
ginn1111 <vanthuanjw@gmail.com>
2024-03-08 00:39:17 +0700

    feat: add go to up btn

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 (function () {
-  addEventListener('DOMContentLoaded', () => {
-    const workExItems = document.querySelectorAll('#resume > ul > li');
+  addEventListener("DOMContentLoaded", () => {
+    const workExItems = document.querySelectorAll("#resume > ul > li");
 
     const intersection = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('in');
+          entry.target.classList.add("in");
         } else {
-          entry.target.classList.remove('in');
+          entry.target.classList.remove("in");
         }
       });
     });
 
     Array.from(workExItems).forEach((item) => {
       intersection.observe(item);
+    });
+
+    document.querySelector(".wrapper").addEventListener("scroll", (e) => {
+      if (e.target.scrollTop > document.body.scrollHeight) {
+        document.querySelector("#go-up-btn").classList.add("active");
+      } else {
+        document.querySelector("#go-up-btn").classList.remove("active");
+      }
+    });
+
+    document.querySelector("#go-up-btn").addEventListener("click", () => {
+      document
+        .querySelector(".wrapper")
+        .scrollTo({ top: 0, behavior: "smooth" });
     });
   });
 })();
